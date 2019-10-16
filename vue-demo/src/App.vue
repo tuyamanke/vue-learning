7ff52276cc3e211f5c0ef4ba7e2c9cfb42d8d26f
tuyamanke <1360273973@qq.com>
2019-10-16 13:58:59 +0800

    子组件通过 props 接收父组件传递的数据（Menu 接收 App 传递的数据）

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,33 +1,42 @@
-<!--主要分为以下三大块-->
-
-<!--用于写 HTML 模板 只能有一个根节点-->
 <template>
   <div>
-    <img src="./assets/logo.png" height="200" width="200"/>
-    <!--3. 使用标签-->
-    <hello-world/>
-    <!--以下写法也行-->
-    <!--<HelloWorld/>-->
+    <!--将父组件中的数据传递给子组件-->
+    <Menu :menus="menus" :webSite="webSite"/>
   </div>
 </template>
 
-<!--配置一些组件对象 export default 等，写 js 代码-->
 <script>
-  // 1. 引入组件
-  import HelloWorld from './components/HelloWorld'
+  import Menu from './components/Menu'
 
   export default {
     name: 'App',
     components: {
-      // 2. 映射成组件标签
-      // HelloWorld: HelloWorld
-      // ES6 写法，属性名和赋给属性的变量名名称一致
-      HelloWorld
+      Menu
+    },
+    data() {
+      return {
+        menus: [{
+          id: 1,
+          name: '科技'
+        }, {
+          id: 2,
+          name: '教育'
+        }, {
+          id: 3,
+          name: '文学'
+        }, {
+          id: 4,
+          name: '艺术'
+        }],
+        webSite: {
+          url: 'https://cn.vuejs.org',
+          title: 'Vue 官网:)'
+        }
+      }
     }
   }
 </script>
 
-<!--写样式，加了 scoped 之后，仅对该组件有效-->
 <style>
 
 </style>
---
+++ vue-demo/src/components/Menu.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <a :href="webSite.url" target="_blank" :title="webSite.title">
+      <img src="../assets/logo.png" height="100" width="100"/>
+    </a>
+    <ul>
+      <!--v-for 时最好要绑定唯一的 key 不然会有麻烦-->
+      <li v-for="(menu, index) in menus" :key="index">
+        <a :href="'http://localhost:8080/menu/' + menu.id">{{menu.name}}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Menu",
+    // 通过 props 声明从父组件接收的数据属性
+    // 之后就可以像自己定义的一样使用了
+    props: ['menus', 'webSite']
+  }
+</script>
+
+<style scoped>
+  ul li {
+    float: left;
+    padding-left: 5px;
+    list-style-type: none;
+  }
+</style>
